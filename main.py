a246e88aa5d96138296db0541518c55448209f80
emerve <emerve@utu.fi>
2023-02-15 15:20:34 +0200

    added from_table_to_games_list( )

--- main.py
+++ main.py
@@ -6,7 +6,61 @@ Functions:
 
 - update_elo_single(...)     = Elo calculator for single game
 - update_elo_tournament(...) = Elo calculator for whole tournament day
+- from_table_to_games_list(...)	= Turns tournament score table in to list of games
 '''
+
+
+def from_table_to_games_list(games_table, verbose=False):
+    """
+	Turns tournament game table in to list of games.
+
+		Variables:
+			games_table: Table of tournament games. Indexes and columns have players names, and in the cells there is info of who won. Eg. ww=white win, bl=black lose, bt=black tie 
+			verbose: if True, print table and games_list
+
+		Returns: List of games, where every games are in format [white_name, black_name, white_result]
+
+	"""
+
+    if verbose: print("Table: \n", games_table)
+
+    # Get players names from tables index and columns (they should be the same).
+    games_table_index = games_table.index
+    games_table_columns = games_table.columns
+
+    # Change table to format:
+    # [white_name, black_name, white_result]
+    # Goes through only the upper triangle in the table.
+    games_list = []
+    for i in range(len(games_table_index)):
+        for j in range(i+1,len(games_table_columns)):
+            
+            # Names of players from index and columns
+            ind = games_table_index[i]
+            col = games_table_columns[j]
+
+            # Reads the cell and appends the game to the games_list in appropriate form.
+            # ww=white win, wt=white tie, wl=white loss, bw=black win, bt=black tie, bl=black lose
+            if games_table.loc[ind,col] == "ww":
+                games_list.append([str(ind), str(col), 1])
+            elif games_table.loc[ind,col] == "wt":
+                games_list.append([str(ind), str(col), 0.5])
+            elif games_table.loc[ind,col] == "wl":
+                games_list.append([str(ind), str(col), 0])
+
+            elif games_table.loc[ind,col] == "bw":
+                games_list.append([str(col), str(ind), 0])
+            elif games_table.loc[ind,col] == "bt":
+                games_list.append([str(col), str(ind), 0.5])
+            elif games_table.loc[ind,col] == "bl":
+                games_list.append([str(col), str(ind), 1])
+
+    if verbose:
+        print("List of games:")
+        for i in games_list:
+            print(i)
+
+    return games_list
 
 
 def update_elo_single(old_elo, opponent_elo, score, n):
